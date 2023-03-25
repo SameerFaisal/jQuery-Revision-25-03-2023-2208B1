b97eeaa32a3d8cf72e0f26a404e49853d8589982
Sameer Faisal <45840879+SameerFaisal@users.noreply.github.com>
2023-03-25 10:07:43 +0500

    25-03-2023

---
+++ jQuery Revision (2208B1)/index.js
@@ -0,0 +1,113 @@
+// $(document).ready(function() {
+//     $("#hide").click(function(){
+//         let time=+prompt("Enter time in seconds")
+// $("div").hide(time*1000)
+//     })
+// })
+
+// $(document).ready(function() {
+//     $("#show").click(function(){
+//         let time=+prompt("Enter time in seconds")
+
+// $("div").show(time*1000)
+//     })
+// })
+
+// $(document).ready(function() {
+//     $("#hide").click(function(){
+//         let time=+prompt("Enter time in seconds")
+// $("div").fadeOut(time*1000)
+//     })
+// })
+
+// $(document).ready(function() {
+//     $("#show").click(function(){
+//         let time=+prompt("Enter time in seconds")
+
+// $("div").fadeIn(time*1000)
+//     })
+// })
+
+// $(document).ready(function() {
+//     $("#hide").click(function(){
+//         let time=+prompt("Enter time in seconds")
+// $("div").slideUp(time*1000)
+//     })
+// })
+
+// $(document).ready(function() {
+//     $("#show").click(function(){
+//         let time=+prompt("Enter time in seconds")
+
+// $("div").slideDown(time*1000)
+//     })
+// })
+
+// $(document).ready(function() {
+//     $("#ch").click(function(){
+//         $("div").addClass("red")
+//     })
+// })
+// $(document).ready(function() {
+//     $("#rm").click(function(){
+//         $("div").removeClass("red")
+//     })
+// })
+
+// $(document).ready(function() {
+//     $("#ch").click(function(){
+//         $("div").toggleClass("red")
+//     })
+// })
+
+
+// $(document).ready(function() {
+//     $("#ch").click(function(){
+//         $("div").css(
+//             {"background-color":"red","border-radius":"200px","width":"100%"}
+//             )
+       
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("#ch").click(function(){
+//       $("div").animate({
+        
+//     height: '+=150px'
+   
+//       },2000);
+//       $("div").animate({
+        
+//     width: '+=150px'
+        
+//       },2000);
+//       $("div").animate({
+        
+//         opacity: '0.8'
+       
+//           },2000);
+//     });
+//   });
+
+// $(document).ready(function(){
+//     $("#get").click(function(){
+//         let fname=$("#text").val()
+//         let lname=$("#text1").val()
+//         alert("The full name is: "+fname+" "+lname)
+
+//     })
+// })
+
+// $(document).ready(function(){
+//     $("#add").click(function(){
+//         let text=prompt("Enter some text")
+//         $("#para").text(text)
+//       })
+// })
+
+// $(document).ready(function(){
+//     $("#at").click(function(){
+// $("#img").attr("src","https://cdn.pixabay.com/photo/2013/10/09/02/27/lake-192990__340.jpg")
+//     })
+// })
